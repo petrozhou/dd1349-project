66beaba22db29011f318accad33052d6fbcc3a44
petrozhou <138794422+petrozhou@users.noreply.github.com>
2026-05-07 19:25:46 +0200

    Adresses #21
    Forgot to commit these last changes for the same issue before closing

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,10 @@ extends CharacterBody2D
 @export var walk_speed = 4.0
 const TILE_SIZE = 16
 
+# Here we refer to AnimationTree
+@onready var anim_tree = $AnimationTree
+@onready var anim_state = anim_tree.get("parameters/playback")
+
 # Player's position on start tile before moving to another tile
 var initial_position = Vector2(0,0)
 
@@ -27,9 +31,11 @@ func _physics_process(delta):
 		process_player_input()
 	# Move if we have an input direction
 	elif input_direction != Vector2.ZERO:
+		anim_state.travel("Walk") # Here we refer to AnimationTree
 		move(delta)
 	# If we don't have an input direction, set to is_moving = false again
 	else:
+		anim_state.travel("Idle") # Here we refer to AnimationTree
 		is_moving = false
 
 # Checks which arrow keys were pressed and sets the movement direction (up/down/left/right)
@@ -38,15 +44,19 @@ func process_player_input():
 	# If we're not moving vertically, THEN check for horizontal key presses/input
 	# Int(true) becomes 1, int(false) becomes 0, so that's how we get -1, 0, or 1
 	if input_direction.y == 0: 
-		input_direction.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+		input_direction.x = int(Input.is_action_pressed("ui_right" )) - int(Input.is_action_pressed("ui_left"))
 	# If we're not moving horizontally, THEN check for vertical key presses/input
 	if input_direction.x == 0:
 		input_direction.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
 	
 	# If we pressed any direction (not zero), save where we started and start moving
 	if input_direction != Vector2.ZERO:
+		anim_tree.set("parameters/Idle/blend_position", input_direction) # Here we refer to AnimationTree
+		anim_tree.set("parameters/Walk/blend_position", input_direction) # Here we refer to AnimationTree
 		initial_position = position
 		is_moving = true
+	else:
+		anim_state.travel("Idle")
 
 # Handles how we actually move between tiles e.g. adds progress each frame, snaps to destination when done, or smoothly moves to "in-between" position
 func move(delta):
