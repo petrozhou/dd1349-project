e8ddc323db6adbef590562b6d6f925a593cf4f47
Daniel Danogw <84149524+Danogw@users.noreply.github.com>
2026-05-10 05:55:13 +0200

    added random chance of entering into battle when stepping into tall grass tile. Resolves #44 , resolves #45, resolves #47 , resolves #36 , resolves #34

--- scripts/tall_grass.gd
+++ scripts/tall_grass.gd
@@ -15,4 +15,7 @@ func _ready() -> void:
 # On when we enter tall grass, we play the "Stepped" tall grass animation
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	anim_player.play("Stepped")
+	#check if the object stepping into the grass is player
+	if body.has_method("try_start_battle"):
+		body.try_start_battle()
 	pass # Replace with function body.
