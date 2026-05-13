77a2a4d3ed754efc0f233d757ebc7f9b2983863a
petrozhou <138794422+petrozhou@users.noreply.github.com>
2026-05-14 01:10:01 +0200

    Fixes #67 and fixes #68
    Added house interior scene
    Added collision bodies around house interior so we cannot walk outside of it
    Added a new door scene for house interior, which leads us to town

--- scripts/door.gd
+++ scripts/door.gd
@@ -1,10 +1,13 @@
 extends Area2D
 
 @export_file() var next_scene_path: String = ""
+@export var is_invisible = false 
 @onready var sprite = $Sprite2D
 @onready var anim_player = $AnimationPlayer
 
 func _ready():
+	if is_invisible:
+		$Sprite2D.texture = null
 	sprite.visible = false
 	var player = find_parent("CurrentScene").get_children().back().find_child("Player")
 	player.player_entering_door_signal.connect(enter_door)
