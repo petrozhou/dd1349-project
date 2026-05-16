98b443ffa4be233343618f1e62189b09cc91ae8f
petrozhou <138794422+petrozhou@users.noreply.github.com>
2026-05-17 01:23:53 +0200

    Fixes #43

--- scripts/tall_grass.gd
+++ scripts/tall_grass.gd
@@ -1,13 +1,26 @@
 extends Node2D
 
+const GrassStepEffect = preload("res://scenes/grass_step_effect.tscn")
+
+var player_inside: bool = false
+
 # Get access to AnimationPlayer 
 @onready var anim_player = $AnimationPlayer
 
 # On when we enter tall grass, we play the "Stepped" tall grass animation
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	player_inside = true
 	anim_player.play("Stepped")
+	player_in_grass()
 	# Check if the object stepping into the grass is player
 	if body.has_method("try_start_battle"):
 		# Wait a tiny bit for the player to finish moving onto the tile before triggering battle
 		await get_tree().create_timer(0.16).timeout
 		body.try_start_battle()
+
+
+func player_in_grass():
+	if player_inside == true:
+		var grass_step_effect = GrassStepEffect.instantiate()
+		grass_step_effect.position = position
+		get_tree().current_scene.add_child(grass_step_effect)
